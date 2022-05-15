f42c475f7fff4cbca6c1c54054bcacff6e2384af
l3nc <faijaisalui@gmail.com>
2022-05-16 01:09:51 +0800

    sepreate the index.js

---
+++ template.js
@@ -0,0 +1,38 @@
+// import { React } from 'react';
+// import { createRoot } from 'react-dom/client';
+
+// const customStyle = { color: '' };
+
+// const greeting = function (greeting) {
+//   const date = new Date(2022, 1, 1, 15);
+//   let currentTime = date.getHours();
+//   if (currentTime < 12) {
+//     customStyle.color = 'red';
+//     return (
+//       <h1 className='heading' style={customStyle}>
+//         Good Morning
+//       </h1>
+//     );
+//   }
+//   if (currentTime < 18) {
+//     customStyle.color = 'green';
+//     return (
+//       <h1 className='heading' style={customStyle}>
+//         Good Afternoon
+//       </h1>
+//     );
+//   } else {
+//     customStyle.color = 'blue';
+//     return (
+//       <h1 className='heading' style={customStyle}>
+//         Good Evening
+//       </h1>
+//     );
+//   }
+// };
+
+// const test1 = greeting(2020, 1, 1, 1);
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(greeting());
