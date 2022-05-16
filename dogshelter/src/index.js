ed04e67cdf8633c1a94bc52aa20742bd9607d2f7
l3nc <faijaisalui@gmail.com>
2022-05-16 18:04:17 +0800

    updated

--- dogshelter/src/index.js
+++ dogshelter/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import { add, subtract, multiply, divide } from './math';
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App />);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>
+);
