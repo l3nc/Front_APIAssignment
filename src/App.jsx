7fbdd9fcc896fcc2cb3d33e75c73126631af9c40
l3nc <faijaisalui@gmail.com>
2022-05-22 18:41:06 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,13 @@
 import React from 'react';
-import { Button } from 'antd';
-import './App.less';
+import { createRoot, ReactDOM } from 'react-dom/client';
+import Frame from './components/Frame';
+import registerForm from './Form/registerForm';
 
-import { Layout, Menu, Breadcrumb } from 'antd';
-import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-const { Header, Content, Footer, Sider } = Layout;
-
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
+export default function App() {
+  return (
+    <div>
+      <Frame />
+      <registerForm />
+    </div>
+  );
 }
-
-const items = [
-  getItem('Sing Up', '1', <PieChartOutlined />),
-  getItem('Charity Login', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [
-    getItem('Team 1', '6'),
-    getItem('Team 2', '8'),
-  ]),
-  getItem('Files', '9', <FileOutlined />),
-];
-
-class SiderDemo extends React.Component {
-  state = {
-    collapsed: false,
-  };
-  onCollapse = (collapsed) => {
-    console.log(collapsed);
-    this.setState({
-      collapsed,
-    });
-  };
-
-  render() {
-    const { collapsed } = this.state;
-    return (
-      <Layout
-        style={{
-          minHeight: '100vh',
-        }}
-      >
-        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-          <div className='logo' />
-          <Menu
-            theme='dark'
-            defaultSelectedKeys={['1']}
-            mode='inline'
-            items={items}
-          />
-        </Sider>
-        <Layout className='site-layout'>
-          <Header
-            className='site-layout-background'
-            style={{
-              padding: 0,
-            }}
-          />
-          <Content
-            style={{
-              margin: '0 16px',
-            }}
-          >
-            <Breadcrumb
-              style={{
-                margin: '16px 0',
-              }}
-            >
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div
-              className='site-layout-background'
-              style={{
-                padding: 24,
-                minHeight: 360,
-              }}
-            >
-              Bill is a cat.
-            </div>
-          </Content>
-          <Footer
-            style={{
-              textAlign: 'center',
-            }}
-          >
-            Ant Design ©2018 Created by Ant UED
-          </Footer>
-        </Layout>
-      </Layout>
-    );
-  }
-}
-
-export default () => <SiderDemo />;
